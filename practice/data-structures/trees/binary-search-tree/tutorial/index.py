6a75c34a169e2ac3b1ed1756496735dcd8435c15
Tyler Long <tyler4long@gmail.com>
2022-02-16 19:01:21 -0800

    First working version of bst practice

--- practice/data-structures/trees/binary-search-tree/tutorial/index.py
+++ practice/data-structures/trees/binary-search-tree/tutorial/index.py
@@ -5,12 +5,12 @@ from typing import Optional
 q = deque(line.strip() for line in sys.stdin)
 
 class Node:
-    def __init__(self, data: int):
+    def __init__(self, data: int) -> None:
         self.data = data
         self.left: Optional[Node] = None
         self.right: Optional[Node] = None
 
-def insert(node: Optional[Node], data: int):
+def insert(node: Optional[Node], data: int) -> Node:
     if node == None:
         return Node(data)
     if data <= node.data:
@@ -24,10 +24,27 @@ root: Optional[Node] = None
 for data in q.popleft().split(' '):
     root = insert(root, int(data))
 
-def print_node(node: Optional[Node], level: int = 0):
+def print_node(node: Optional[Node], level: int = 0) -> None:
     if node == None:
         return
     print_node(node.right, level + 1)
     print('{indent}==>{data}'.format(indent = ' ' * 4 * level, data = node.data))
     print_node(node.left, level + 1)
-print_node(root)
+# print_node(root)
+
+query = int(q.popleft())
+
+def find_node(node: Optional[Node], data: int) -> Optional[Node]:
+    if node == None:
+        return None
+    if node.data == data:
+        return node
+    return find_node(node.left, data) or find_node(node.right, data)
+
+def output_node(node: Optional[Node]) -> None:
+    if node == None:
+        return
+    print(node.data)
+    output_node(node.left)
+    output_node(node.right)
+output_node(find_node(root, query))
