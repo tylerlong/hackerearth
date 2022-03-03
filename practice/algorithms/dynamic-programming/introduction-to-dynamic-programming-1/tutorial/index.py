3ef40f579270bc7fada9f46eaa16edb25fe973c4
Tyler Liu <tyler4long@gmail.com>
2022-03-02 20:24:53 -0800

    Refactor code

--- practice/algorithms/dynamic-programming/introduction-to-dynamic-programming-1/tutorial/index.py
+++ practice/algorithms/dynamic-programming/introduction-to-dynamic-programming-1/tutorial/index.py
@@ -7,13 +7,13 @@ inputs.popleft()
 numbers = [int(i) for i in inputs]
 n = max(numbers)
 arr = [0] * (n + 1)
-arr[1] = 1
-arr[2] = 2
-arr[3] = 6
+arr[0] = 1
 
-for i in range(4, n + 1):
-  arr[i] = i * arr[i - 1]
 
 mod = 10 ** 9 + 7
+for i in range(1, n + 1):
+  arr[i] = i * arr[i - 1] % mod
+
+
 for i in numbers:
-  print(arr[i] % mod)
+  print(arr[i])
